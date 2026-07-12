22164f62f9a7255915a12cb0f494854a1d3b4322
Miniflint <tristan.goel@gmail.com>
2026-07-13 01:52:59 +0200

    changed python

--- lil_scripts/parse_code_rfc.py
+++ lil_scripts/parse_code_rfc.py
@@ -2,12 +2,15 @@ def main():
     with open("error_codes.rfc", "r") as f:
         t:list[str] = f.readlines()
     for line in t:
-        #print(line)
         splitted = line.split(" \t")
         code = splitted[0]
         msg = splitted[1]
-        test = splitted[2]
-        if (len(test) > 6 and test[0] == 'R'):
-            print(f"\t{msg} = {code},")
+        rfc = splitted[2]
+        try: 
+            formatting = splitted[3]
+        except:
+            continue
+        if (len(rfc) > 6 and rfc[0] == 'R'):
+            print(f"\t{msg} = {code}, //{formatting}")
 
 main()
