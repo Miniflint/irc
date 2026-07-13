1574ed6d018c0e82481c086fecb1092ad43d6b57
Miniflint <tristan.goel@gmail.com>
2026-07-13 16:05:55 +0200

    started replyMsg

---
+++ srcs/Class/Server/replyMsg.cpp
@@ -0,0 +1,642 @@
+#include "Server.hpp"
+
+bool Server::handleRplWelcome(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplYourhost(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplCreated(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplMyinfo(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplBounce(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplTracelink(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplTraceconnecting(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplTracehandshake(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplTraceunknown(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplTraceoperator(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplTraceuser(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplTraceserver(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplTraceservice(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplTracenewtype(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplTraceclass(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplTracereconnect(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplStatslinkinfo(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplStatscommands(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplStatscline(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplStatsnline(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplStatsiline(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplStatskline(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplStatsqline(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplStatsyline(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplEndofstats(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplUmodeis(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplServiceinfo(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplEndofservices(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplService(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplServlist(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplServlistend(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplStatsvline(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplStatslline(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplStatsuptime(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplStatsoline(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplStatshline(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplStatsping(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplStatsbline(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplStatsdline(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplLuserclient(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplLuserop(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplLuserunknown(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplLuserchannels(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplLuserme(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplAdminme(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplAdminloc1(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplAdminloc2(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplAdminemail(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplTracelog(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplTraceend(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplTryagain(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplNone(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplAway(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplUserhost(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplIson(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplUnaway(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplNowaway(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplWhoisuser(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplWhoisserver(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplWhoisoperator(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplWhowasuser(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplEndofwho(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplWhoischanop(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplWhoisidle(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplEndofwhois(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplWhoischannels(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplListstart(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplList(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplListend(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplChannelmodeis(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplUniqopis(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplNotopic(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplTopic(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplInviting(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplSummoning(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplInvitelist(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplEndofinvitelist(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplExceptlist(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplEndofexceptlist(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplVersion(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplWhoreply(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplNamreply(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplKilldone(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplClosing(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplCloseend(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplLinks(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplEndoflinks(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplEndofnames(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplBanlist(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplEndofbanlist(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplEndofwhowas(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplInfo(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplMotd(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplInfostart(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplEndofinfo(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplMotdstart(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplEndofmotd(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplYoureoper(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplRehashing(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplYoureservice(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplMyportis(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplTime(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplUsersstart(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplUsers(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplEndofusers(Client &c)
+{
+    (void)c;
+}
+bool Server::handleRplNousers(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNosuchnick(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNosuchserver(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNosuchchannel(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrCannotsendtochan(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrToomanychannels(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrWasnosuchnick(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrToomanytargets(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNosuchservice(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNoorigin(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNorecipient(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNotexttosend(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNotoplevel(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrWildtoplevel(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrBadmask(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrUnknowncommand(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNomotd(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNoadmininfo(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrFileerror(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNonicknamegiven(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrErroneusnickname(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNicknameinuse(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNickcollision(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrUnavailresource(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrUsernotinchannel(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNotonchannel(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrUseronchannel(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNologin(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrSummondisabled(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrUsersdisabled(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNotregistered(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNeedmoreparams(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrAlreadyregistered(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNopermforhost(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrPasswdmismatch(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrYourebannedcreep(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrYouwillbebanned(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrKeyset(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrChannelisfull(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrUnknownmode(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrInviteonlychan(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrBannedfromchan(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrBadchannelkey(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrBadchanmask(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNochanmodes(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrBanlistfull(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNoprivileges(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrChanoprivsneeded(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrCantkillserver(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrRestricted(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrUniqoprivsneeded(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNooperhost(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrNoservicehost(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrUmodeunknownflag(Client &c)
+{
+    (void)c;
+}
+bool Server::handleErrUsersdontmatch(Client &c)
+{
+    (void)c;
+}
